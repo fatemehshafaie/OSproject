a4d35c267f6947cc579eb3c4f92d5c1f8db372e1
unknown <fatemehshefahi@gmail.com>
2024-04-20 14:48:06 +0330

    os homework3

---
+++ os33/os33/Program.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using os3;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OSLabEx3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<person> persons = new List<person>();
+            Console.WriteLine("1.set info for new people");
+            Console.WriteLine("2.get info");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter number of new people");
+                    int count = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i < count; i++)
+                    {
+                        person person = new person();
+                        Console.WriteLine("Enter Name :");
+                        person.Name = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name :");
+                        person.LastName = Console.ReadLine();
+                        Console.WriteLine("Enter ssd :");
+                        person.ssd = Console.ReadLine();
+                        Console.WriteLine("Enter Phone number :");
+                        person.phoneNumber = Console.ReadLine();
+                        Console.WriteLine("Enter Age :");
+                        person.Age = Convert.ToInt32(Console.ReadLine());
+                        persons.Add(person);
+                    }
+                    string json = JsonConvert.SerializeObject(persons, Formatting.Indented);
+                    File.WriteAllText("people.json", json);
+                    Console.WriteLine("people info saved in json file");
+                    break;
+
+                case 2:
+                    try
+                    {
+                        string jsonText = File.ReadAllText("people.json");
+                        persons = JsonConvert.DeserializeObject<List<person>>(jsonText);
+                        if (persons != null)
+                        {
+                            persons.AddRange(persons);
+                            Console.WriteLine("people info getting from json file");
+                            for (int i = 0; i < persons.Count; i++)
+                            {
+                                Console.WriteLine(persons[i].Name + " , " + persons[i].LastName + " , " + persons[i].ssd + " , " + persons[i].phoneNumber + " , " + persons[i].Age + " , ");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No person info found");
+                        }
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine("json file not found");
+                    }
+                    break;
+
+            }
+        }
+    }
+}
---
+++ os33/os33/person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace os3
+{
+    public struct person
+    {
+        public string Name;
+        public string LastName;
+        public string ssd;
+        public string phoneNumber;
+        public int Age;
+    }
+}
